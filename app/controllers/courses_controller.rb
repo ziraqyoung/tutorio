1594ec9a3d53d38856ff4eb6e91e833c3cf83a0c
ziraqyoung <ziraqyoung@outlook.com>
2020-03-27 00:18:17 +0300

    - Define courses_for_branch method for branch-specific courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,17 +1,26 @@
 class CoursesController < ApplicationController
   def development
-    @courses = Course.by_branch('Development')
+    courses_for_branch(params[:action])
   end
 
   def design
+    courses_for_branch(params[:action])
   end
 
   def photography
+    courses_for_branch(params[:action])
   end
 
   def marketing
+    courses_for_branch(params[:action])
   end
 
   def business
+    courses_for_branch(params[:action])
   end
+
+  private
+    def courses_for_branch(branch)
+      @courses = Course.by_branch(branch)
+    end
 end
