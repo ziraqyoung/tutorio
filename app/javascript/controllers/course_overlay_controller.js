8392f48bf309adad8989f3883bcfb92723d8e5b6
ziraqyoung <ziraqyoung@outlook.com>
2020-04-02 19:56:23 +0300

    - Add hover styles in courses.scss - In _branch.html.haml wrap courses collection rendering in data-controller='course-overlay' - In _course.html.haml, convert wrapper div into a tag -   > Remove redudant data-target attrs -

--- app/javascript/controllers/course_overlay_controller.js
+++ app/javascript/controllers/course_overlay_controller.js
@@ -2,20 +2,10 @@ import { Controller } from 'stimulus';
 import tippy from 'tippy.js';
 
 export default class extends Controller {
-  static targets = ['overlay']
+  static targets = ['trigger']
 
   showOverlay(event) {
-    const overlays = document.querySelectorAll('.course-overlay-selected');
-
-    if(overlays.length > 1) {
-      for (let i = 0; i < overlays.length; ++i) {
-        overlays[i].classList.remove('course-overlay-selected');
-      }
-    }
-
-    this.overlayTarget.classList.toggle('course-overlay-selected');
-  }
-
-  hideOverlays(event) {
+    event.preventDefault();
+    console.log(this.element.querySelectorAll('a.overlay'));
   }
 }
