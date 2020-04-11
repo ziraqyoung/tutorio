9c16a58a98bd4c335b36bb1d194e04be6034715a
ziraqyoung <ziraqyoung@outlook.com>
2020-04-11 17:49:33 +0300

    Split categories into partial - Make landing page tabs functional

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,19 @@
 class LandingController < ApplicationController
   def index
-    @top_courses = Course.limit(8)
     @branches = Category.distinct.select(:branch)
+
+    if params[:branch].present? && @branches.include?(params[:branch])
+      @top_courses = Course.by_branch(params[:branch]).limit(8)
+      respond_to do |format|
+        format.html { redirect_to root_url(branch: params[:branch])  }
+        format.js { render partial: 'landing/landing_courses' }
+      end
+    else
+      @top_courses = Course.limit(8)
+      respond_to do |format|
+        format.html { render :index }
+        format.js { render partial: 'landing/landing_courses' }
+      end
+    end
   end
 end
